5587f78877f6cebe805c437bdac11a67e6edd84d
Ben Newman <ben@meteor.com>
2015-07-09 23:08:33 -0400

    Add an ecmascript core package.
    This package will be installed by default for all new apps and packages.

---
+++ packages/ecmascript/package.js
@@ -0,0 +1,21 @@
+Package.describe({
+  name: 'ecmascript',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.registerBuildPlugin({
+  name: 'compile-ecmascript',
+  use: ['compiler-plugin', 'babel-compiler@5.6.17'],
+  sources: ['plugin.js']
+});
+
+Package.onUse(function(api) {
+  api.imply('babel-runtime@0.1.0');
+});
